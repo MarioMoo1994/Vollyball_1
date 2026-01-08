89c009b0833a6d300651c9008e0760bc9d21b451
MarioMoo1994 <mariusalny@gmail.com>
2026-01-08 17:31:49 +0100

    lyd

---
+++ Assets/Scripts/AudioManager.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+
+public class AudioManager : MonoBehaviour
+{
+    [SerializeField] AudioSource SFXSource;
+    [SerializeField] AudioSource MusicSource;
+    public AudioClip Hit;
+    public AudioClip Whistle;
+    public AudioClip Ambiance;
+
+        private void Start()
+    {
+        MusicSource.clip = Ambiance;
+        MusicSource.Play();
+    }
+
+    public void PlaySFX(AudioClip clip)
+    {
+        SFXSource.PlayOneShot(clip);
+    }
+}
